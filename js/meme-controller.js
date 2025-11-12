7238839076303fff071a6005ef5ff73cba6b894d
Shaked Livyani <shakedliv38@gmail.com>
2025-11-12 11:34:56 +0200

    Add renderMeme function

---
+++ js/meme-controller.js
@@ -0,0 +1,25 @@
+'use strict'
+let gElCanvas
+let gCtx
+function renderMeme(elImg) {
+    gElCanvas = document.querySelector('.meme-canvas')
+    gCtx = gElCanvas.getContext('2d')
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+    drawText('hello', gElCanvas.width / 2, gElCanvas.height / 6)
+}
+
+
+
+
+function drawText(text, x, y) {
+  gCtx.beginPath()
+  gCtx.lineWidth = 2
+  gCtx.strokeStyle = 'brown'
+  gCtx.fillStyle = 'black'
+  gCtx.font = '40px Arial'
+  gCtx.textAlign = 'center'
+  gCtx.textBaseline = 'middle'
+
+  gCtx.fillText(text, x, y)
+  gCtx.strokeText(text, x, y)
+}
